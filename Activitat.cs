5c491974fa416142a92f0b86c52283b7e292b37b
DAVIDaM2202 <38979076+DAVIDaM2202@users.noreply.github.com>
2019-05-31 12:33:50 +0200

    Add files via upload

---
+++ Activitat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Text;
+namespace ConsoleApp1.Controller
+{
+    class Activitat : Base
+    {
+        public async void Actividad(object sender, EventArgs e)
+        {
+            string ruta = @"c:\temp\Backup.txt";
+            if (File.Exists(ruta))
+            {
+                File.Delete(ruta);
+            }
+            var x = GetActividades();
+            FileStream fs = File.Create(ruta);
+            Byte[] info = new UTF8Encoding(true).GetBytes(x);
+            fs.Write(info, 0, info.Length);
+
+        }
+    }
+}
